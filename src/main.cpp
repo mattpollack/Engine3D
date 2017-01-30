09905363ccd7f03734e8bca599dd8c4588a5e5fb
Matthew Pollack <matt.pck@gmail.com>
2017-01-29 22:16:44 -0500

    Removing readme comment from main

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,3 @@
-/**
- * PREMISE
- * Gain reputation as you complete dungeons by travelling around a procedurally
- * generated world. Gaining reputation is like gaining experience. Villages and
- * NPC generate responses proportional to local reputation. Emphasis on
- * survival, and crafting.
- *
- * TENTATIVE GOALS
- * - Generate terrain based on character position
- * - Move along terrain endlessly
- *
- * TODO
- * - TODO
- */
-
 #include "SceneManager.h"
 #include "Scene.h"
 #include "Scenes/TestScene.h"
